3d76d349b5a531789e556dc5ad05d6302ccff43d
khongcoten002 <minhb2306674@student.ctu.edu.vn>
2024-09-20 16:31:04 +0700

    Delete src/config.js

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  NEWS_API: "a9a54bdf1f254ed3ad8585ab7e0ea7a5", // News API key
-  AI_KEY: "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", // Hugging Face API key
-};
-
-export default config;
